92279d0888f5c539231a5d8bb666babf2579aee1
szhao045 <siqi_zhao@live.com>
2022-01-24 13:38:30 -0600

    sci RNA seq pipeline start

---
+++ sciRNAseq_pipeline/sci_main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# define the fastq folder including all the fastq files
+fastq_folder = "/scratch/bclab/szhao/220124_sciRNA_tech/fastq"
+
+# define the PCR group sample id for each fastq file
+
